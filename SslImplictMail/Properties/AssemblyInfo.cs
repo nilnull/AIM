937dc02e6c30b9946abf519ac231451c10cc7905
nilnull <a.farhang.d@gmail.com>
2014-09-20 18:27:31 -0700

    Change project properties for sourceforg project

--- SslImplictMail/Properties/AssemblyInfo.cs
+++ SslImplictMail/Properties/AssemblyInfo.cs
@@ -1,23 +1,24 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SslImplicitMail")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Aegis Implicit Mail")]
+[assembly: AssemblyDescription("Open Source implicit and explicit mime and smime mail library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Aegis Solutions")]
 [assembly: AssemblyProduct("SslImplicitMail")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Araz Farhang Dareshuri  2014")]
+[assembly: AssemblyTrademark("Aegis")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2d918e83-0307-4931-b029-9273b3a4e9ee")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
