e049850f3fd7cd6476ddc1c0688cec97f1034637
Araz Farhang Da <a.farhang.d@gmail.com>
2022-02-05 00:37:43 +0800

    Added nuget release

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,4 +5,4 @@ dotnet pack ".\AegisImplictMail\AegisImplicitMail.csproj" -c Release
 
 
 echo "Pushing..."
-dotnet nuget push ./AegisImplictMail/bin/Release/AIM.*.nupkg  -k $NUGET_API -s https://api.nuget.org/v3/index.json
+dotnet nuget push ./AegisImplictMail/bin/Release/AIM.*.nupkg  -k $NUGET_API -Source https://api.nuget.org/v3/index.json
